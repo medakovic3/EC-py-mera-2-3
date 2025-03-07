11f9190c7045a2fc3cb31f55d0cfc5174602a07d
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-07 15:47:10 +0100

    Add output data attributes

--- frontend/console.py
+++ frontend/console.py
@@ -5,3 +5,19 @@ class Console:
 		self.annual_cost_savings = None
 		self.annual_final_energy_savings = None
 		self.co2_emission_reduction = None
+  
+		# Input data
+		self.municipality = None
+		self.construction_period = None
+		self.dwelling_type = None
+		self.building_type = None
+		self.floor_area = None
+		self.heating_system_type = None
+		self.heating_fuel_type = None
+		self.annual_fuel_consumption = None
+		self.fuel_cost_per_unit = None
+		self.investment_cost = None
+		self.insulated_construction_type = None
+		self.insulation_thickness = None
+		self.insulated_area = None
+		self.insulation_thermal_conductivity = None
