e963b4203c77655787ff0db421298b3355f00f52
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-07 15:14:23 +0100

    Create Console class with output data attributes

---
+++ frontend/console.py
@@ -0,0 +1,7 @@
+class Console:
+	def __init__(self):
+		# Output data
+		self.payback_period = None
+		self.annual_cost_savings = None
+		self.annual_final_energy_savings = None
+		self.co2_emission_reduction = None
