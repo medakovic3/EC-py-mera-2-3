fef711f3437f1c4f3bee84f6f24a7fab48a2f0fa
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-08 12:49:15 +0100

    Use created data structures for Console class attributes
    Make them Optional to inform that values can be None and are going to be
    set later on

--- frontend/console.py
+++ frontend/console.py
@@ -1,23 +1,8 @@
+from typing import Optional
+from io_dataclass import *
+
 class Console:
 	def __init__(self):
-		# Output data
-		self.payback_period = None
-		self.annual_cost_savings = None
-		self.annual_final_energy_savings = None
-		self.co2_emission_reduction = None
-  
-		# Input data
-		self.municipality = None
-		self.construction_period = None
-		self.dwelling_type = None
-		self.building_type = None
-		self.floor_area = None
-		self.heating_system_type = None
-		self.heating_fuel_type = None
-		self.annual_fuel_consumption = None
-		self.fuel_cost_per_unit = None
-		self.investment_cost = None
-		self.insulated_construction_type = None
-		self.insulation_thickness = None
-		self.insulated_area = None
-		self.insulation_thermal_conductivity = None
+		self.output_data: Optional[OutputData] = None
+		self.user_house_info: Optional[UserHouseInfo] = None
+		self.insulation_info: Optional[InsulationInfo] = None
