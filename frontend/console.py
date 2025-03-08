8c45ccf45c11ff775533debeb5a5f3b9e2a35049
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-08 14:35:20 +0100

    Define run function with empty sub-functions bodies

--- frontend/console.py
+++ frontend/console.py
@@ -6,3 +6,16 @@ class Console:
 		self.output_data: Optional[OutputData] = None
 		self.user_house_info: Optional[UserHouseInfo] = None
 		self.insulation_info: Optional[InsulationInfo] = None
+
+	def run(self):
+		self.generate_input_data()
+		# TODO Calculate output data
+		self.print_output_data()
+
+	def generate_input_data(self):
+		# TODO
+		None
+
+	def print_output_data(self):
+		# TODO
+		None
