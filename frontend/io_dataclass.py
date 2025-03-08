ed4a9aec4264636cdbe5b16508d6c8b80cced15d
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-08 12:43:55 +0100

    Create structure for user's house information

--- frontend/io_dataclass.py
+++ frontend/io_dataclass.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from input_options import *
 
 @dataclass
 class OutputData:
@@ -6,3 +7,15 @@ class OutputData:
     payback_period: float
     annual_final_energy_savings: float
     co2_emission_reduction: float
+
+@dataclass
+class UserHouseInfo:
+    municipality: Municipality
+    construction_period: ConstructionPeriod
+    dwelling_type: DwellingType
+    building_type: BuildingType
+    floor_area: float
+    heating_system_type: HeatingSystemType
+    heating_fuel_type: HeatingFuelType
+    annual_fuel_consumption: float
+    fuel_cost_per_unit: float
