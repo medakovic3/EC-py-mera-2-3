9bf1dc8df07ea838954e6a0b0598a5a8e438009d
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-08 11:53:51 +0100

    Add Enum for building type

--- frontend/input_options.py
+++ frontend/input_options.py
@@ -4,3 +4,8 @@ class DwellingType(Enum):
 	APARTMENT = "Apartment"
 	DETACHED_HOUSE = "Detached House"
 	SEMI_DETACHED_HOUSE = "Semi-Detached or Row House"
+
+class BuildingType(Enum):
+    DETACHED = "Detached"
+    BLOCK = "Block"
+    HIGH_RISE = "High-Rise"
