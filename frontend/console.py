8ebe0509e79cf7cb3ce29e1f0868f7c1c995d97d
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-08 14:50:07 +0100

    Define generate_input_data() method
    This method purpose is to enable testing before reading input from
    console is implemented

--- frontend/console.py
+++ frontend/console.py
@@ -13,8 +13,25 @@ class Console:
 		self.print_output_data()
 
 	def generate_input_data(self):
-		# TODO
-		None
+		self.user_house_info = UserHouseInfo(
+			municipality			= Municipality.VOZDOVAC,
+			construction_period		= ConstructionPeriod.PERIOD_1991_2012,
+			dwelling_type			= DwellingType.APARTMENT,
+			building_type			= BuildingType.HIGH_RISE,
+			floor_area				= 60.0,
+			heating_system_type		= HeatingSystemType.CENTRAL,
+			heating_fuel_type		= HeatingFuelType.DISTRICT_HEATING,
+			annual_fuel_consumption	= None,
+			fuel_cost_per_unit		= None
+		)
+
+		self.insulation_info = InsulationInfo(
+			investment_cost					= 300000.0,
+			insulated_construction_type		= InsulatedConstructionType.EXTERNAL_WALL,
+			insulation_thickness			= 15.0,
+			insulated_area					= 30.0,
+			insulation_thermal_conductivity	= 0.035
+		)
 
 	def print_output_data(self):
 		# TODO
