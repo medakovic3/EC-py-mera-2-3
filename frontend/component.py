9e268f249dfaad7c18601bd0bfc0175bca16cb77
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-10 15:45:10 +0100

    Initialize output_data in Component class

--- frontend/component.py
+++ frontend/component.py
@@ -2,6 +2,6 @@ from frontend.io_dataclass import *
 
 class Component:
     def __init__(self):
-        self.output_data: OutputData = None
+        self.output_data: OutputData = OutputData()
         self.user_house_info: UserHouseInfo = None
         self.insulation_info: InsulationInfo = None
