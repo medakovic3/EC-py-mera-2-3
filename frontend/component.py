2d65e83d247cb4f3513643ceda0a7b19f1bdd960
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-10 15:50:57 +0100

    Define method for output data calculation

--- frontend/component.py
+++ frontend/component.py
@@ -5,3 +5,21 @@ class Component:
         self.output_data: OutputData = OutputData()
         self.user_house_info: UserHouseInfo = None
         self.insulation_info: InsulationInfo = None
+
+    def calculate_output_data(
+        self,
+        user_house_info: UserHouseInfo,
+        insulation_info: InsulationInfo
+    ) -> OutputData:
+        self.set_input_info(user_house_info, insulation_info)
+        # TODO Get data from databse
+        self.start_calculation()
+        return self.output_data
+    
+    def set_input_info(self):
+        # TODO
+        pass
+
+    def start_calculation(self):
+        # TODO
+        pass
