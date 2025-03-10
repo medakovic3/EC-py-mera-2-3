3ae49a204f76e8a4f7e35c721aac081aec36c6d7
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-10 15:05:07 +0100

    Create Component class with data attributes

---
+++ frontend/component.py
@@ -0,0 +1,7 @@
+from frontend.io_dataclass import *
+
+class Component:
+    def __init__(self):
+        self.output_data: OutputData = None
+        self.user_house_info: UserHouseInfo = None
+        self.insulation_info: InsulationInfo = None
