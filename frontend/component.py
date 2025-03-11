be9935e09adb1168c5a93d1ec791f90b9efb0267
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-11 17:44:28 +0100

    Add database attributes

--- frontend/component.py
+++ frontend/component.py
@@ -1,10 +1,17 @@
 from frontend.io_dataclass import *
+from frontend.model.heating_fuel_parameters_model import HeatingFuelParameters
+from frontend.model.insulated_surface_parameters_model import InsulatedSurfaceParameters
 
 class Component:
     def __init__(self):
         self.output_data: OutputData = OutputData()
         self.user_house_info: UserHouseInfo = None
         self.insulation_info: InsulationInfo = None
+
+        self.needed_energy_per_m2: float = 0.0
+        self.hdd: float = 0.0
+        self.heating_fuel: HeatingFuelParameters = None
+        self.insulated_surface: InsulatedSurfaceParameters = None
 
     def calculate_output_data(
         self,
