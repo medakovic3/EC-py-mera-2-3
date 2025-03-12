7dce7b80d35bdd2cc2c2c012c5334db6083e90b3
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-12 22:39:38 +0100

    Connect to a local MongoDB database

---
+++ backend/db_connection.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+
+MONGO_URI = "mongodb://localhost:27017/"
+DATABASE_NAME = "EnergyCalculator"
+
+client = MongoClient(MONGO_URI)
+db = client[DATABASE_NAME]
+
+def get_db():
+    return db
+
+# Test database connection
+collections = db.list_collection_names()
+print("Collections in database:", collections)
