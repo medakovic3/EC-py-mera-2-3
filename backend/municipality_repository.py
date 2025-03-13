0d6a46e05c63f8b75baf03fa2a1af27d991b6f82
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-13 12:59:36 +0100

    Define repository method for getting hdd

--- backend/municipality_repository.py
+++ backend/municipality_repository.py
@@ -4,3 +4,7 @@ class MunicipalityRepository:
     def __init__(self):
         self.db = get_db()
         self.collection = self.db["Municipality"]
+
+    def get_hdd_by_municipality(self, municipality_name: str) -> float:
+        municipality_document = self.collection.find_one({"name": municipality_name})
+        return municipality_document["hdd"]
