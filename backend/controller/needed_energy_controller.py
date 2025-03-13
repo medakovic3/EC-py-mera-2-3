05a011a4b71fc7ad02cca6cd64acf36b2c2481fc
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-13 16:41:32 +0100

    Define controller method for getting needed energy

--- backend/controller/needed_energy_controller.py
+++ backend/controller/needed_energy_controller.py
@@ -1,2 +1,10 @@
 class NeededEnergyController:
-    pass
+    def get_needed(
+        self,
+        construction_period: str,
+        insulated_construction_type: str
+    ) -> float:
+        return self.needed_energy_repository.get_needed(
+            construction_period,
+            insulated_construction_type
+        )
