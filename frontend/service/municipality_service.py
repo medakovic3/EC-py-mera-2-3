5695c31735fcc185d1d333e45b2c162101f1ad4f
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-13 14:11:15 +0100

    Define service method for getting hdd

--- frontend/service/municipality_service.py
+++ frontend/service/municipality_service.py
@@ -3,3 +3,6 @@ from backend.controller.municipality_controller import MunicipalityController
 class MunicipalityService:
     def __init__(self):
         self.controller = MunicipalityController()
+
+    def get_hdd_by_municipality(self, municipality_name: str) -> float:
+        return self.controller.get_hdd_by_municipality(municipality_name)
