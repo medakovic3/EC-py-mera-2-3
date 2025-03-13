df72a47a7b8202bc8833b10abb7ed49239efa34c
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-13 13:17:14 +0100

    Define controller method for getting hdd

--- backend/controller/municipality_controller.py
+++ backend/controller/municipality_controller.py
@@ -3,3 +3,6 @@ from backend.repository.municipality_repository import MunicipalityRepository
 class MunicipalityController:
     def __init__(self):
         self.repository = MunicipalityRepository()
+
+    def get_hdd_by_municipality(self, municipality_name: str) -> float:
+        return self.repository.get_hdd_by_municipality(municipality_name)
