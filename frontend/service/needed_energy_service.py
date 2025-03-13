baff5c1fbd5a28091b6e4fc7319735895f3c16f1
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-13 15:02:47 +0100

    Define service method for getting needed energy

--- frontend/service/needed_energy_service.py
+++ frontend/service/needed_energy_service.py
@@ -1,2 +1,10 @@
 class NeededEnergyService:
-    pass
+    def get_needed(
+        self,
+        construction_period: str,
+        insulated_construction_type: str
+    ) -> float:
+        return self.controller.get_needed(
+            construction_period,
+            insulated_construction_type
+        )
