0e2f80df3f3235d3c203be6e0bf014d99e9474c0
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-13 16:57:46 +0100

    Define repository method for getting needed energy

--- backend/repository/needed_energy_repository.py
+++ backend/repository/needed_energy_repository.py
@@ -1,8 +1,14 @@
+from backend.db_connection import get_db
+
 class NeededEnergyRepository:
+    def __init__(self):
+        self.db = get_db()
+        self.collection = self.db["NeededEnergyPerM2"]
+
     def get_needed(
         self,
         construciton_period: str,
         insulated_surface_type: str
     ) -> float:
-        needed_energy_document = collection.find_one({"construction_period": construciton_period})
+        needed_energy_document = self.collection.find_one({"construction_period": construciton_period})
         return needed_energy_document[insulated_surface_type]
