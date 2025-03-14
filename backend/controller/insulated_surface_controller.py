6efcaee0638ff3c951f48d82eacbbbdb7a7d5291
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-14 14:04:52 +0100

    Define controller method for getting insulated surface params

--- backend/controller/insulated_surface_controller.py
+++ backend/controller/insulated_surface_controller.py
@@ -1,2 +1,13 @@
+from frontend.model.insulated_surface_parameters_model import InsulatedSurfaceParameters
+
+
 class InsulatedSurfaceController:
-    pass
+    def get_insulated_surface_parameters(
+        self,
+        insulated_surface_type: str,
+        construction_period: str
+    ) -> InsulatedSurfaceParameters:
+        return self.repository.get_insulated_surface_parameters(
+            insulated_surface_type,
+            construction_period
+        )
