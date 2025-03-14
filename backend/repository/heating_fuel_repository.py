06d30694dfdeb8b3244204b44629593d6e6fe45b
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-14 15:09:17 +0100

    Declare repo method for getting fuel params

--- backend/repository/heating_fuel_repository.py
+++ backend/repository/heating_fuel_repository.py
@@ -1,6 +1,14 @@
 from backend.db_connection import get_db
+from frontend.model.heating_fuel_parameters_model import HeatingFuelParameters
 
 class HeatingFuelRepository:
     def __init__(self):
         self.db = get_db()
         self.collection = self.db["HeatingFuel"]
+
+    def get_heating_fuel_parameters(
+        self,
+        heating_fuel_type: str,
+        heating_system_type: str
+    ) -> HeatingFuelParameters:
+        pass
