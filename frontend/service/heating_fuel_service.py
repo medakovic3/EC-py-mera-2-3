c5253b100a2fa229b02b70e0c41bcbf854bf8264
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-14 15:25:00 +0100

    Define service method for getting heating fuel params

--- frontend/service/heating_fuel_service.py
+++ frontend/service/heating_fuel_service.py
@@ -1,5 +1,16 @@
 from backend.controller.heating_fuel_controller import HeatingFuelController
+from frontend.model.heating_fuel_parameters_model import HeatingFuelParameters
 
 class HeatingFuelService:
     def __init__(self):
         self.controller = HeatingFuelController()
+
+    def get_heating_fuel_parameters(
+        self,
+        heating_fuel_type: str,
+        heating_system_type: str
+    ) -> HeatingFuelParameters:
+        return self.controller.get_heating_fuel_parameters(
+            heating_fuel_type,
+            heating_system_type
+        )
