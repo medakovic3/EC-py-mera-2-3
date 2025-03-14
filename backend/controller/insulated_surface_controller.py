2bd95e33dd842f9e794f6b5a8d13316f8318d140
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-14 14:08:49 +0100

    Import insulated surface repository into controller

--- backend/controller/insulated_surface_controller.py
+++ backend/controller/insulated_surface_controller.py
@@ -1,7 +1,11 @@
 from frontend.model.insulated_surface_parameters_model import InsulatedSurfaceParameters
+from backend.repository.insulated_surface_repository import InsulatedSurfaceRepository
 
 
 class InsulatedSurfaceController:
+    def __init__(self):
+        self.repository = InsulatedSurfaceRepository()
+
     def get_insulated_surface_parameters(
         self,
         insulated_surface_type: str,
