19b47bbc358c9719c296bafca19982c61a069a92
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-14 14:16:50 +0100

    Add database connection to repository

--- backend/repository/insulated_surface_repository.py
+++ backend/repository/insulated_surface_repository.py
@@ -1,6 +1,11 @@
 from frontend.model.insulated_surface_parameters_model import InsulatedSurfaceParameters
+from backend.db_connection import get_db
 
 class InsulatedSurfaceRepository:
+    def __init__(self):
+        self.db = get_db()
+        self.collection = self.db["InsulatedSurface"]
+
     def get_insulated_surface_parameters(
         self,
         insulated_surface_type: str,
