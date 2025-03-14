9f1f6467657968bdf0db8e9e396ccc06314b7d4a
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-14 13:58:58 +0100

    Define service method for getting insulated surface params

--- frontend/service/insulated_surface_service.py
+++ frontend/service/insulated_surface_service.py
@@ -1,2 +1,12 @@
+from frontend.model.insulated_surface_parameters_model import InsulatedSurfaceParameters
+
 class InsulatedSurfaceService:
-    pass
+    def get_insulated_surface_parameters(
+        self,
+        insulated_surface_type: str,
+        construciton_period: str
+    ) -> InsulatedSurfaceParameters:
+        return self.controller.get_insulated_surface_parameters(
+            insulated_surface_type,
+            construciton_period
+        )
