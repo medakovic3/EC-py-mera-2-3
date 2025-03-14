f3d8ee5ef478b559b85e9dcdec516b5fe3568939
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-14 14:00:41 +0100

    Import insulated surface controller to service

--- frontend/service/insulated_surface_service.py
+++ frontend/service/insulated_surface_service.py
@@ -1,6 +1,10 @@
 from frontend.model.insulated_surface_parameters_model import InsulatedSurfaceParameters
+from backend.controller.insulated_surface_controller import InsulatedSurfaceController
 
 class InsulatedSurfaceService:
+    def __init__(self):
+        self.controller = InsulatedSurfaceController()
+
     def get_insulated_surface_parameters(
         self,
         insulated_surface_type: str,
