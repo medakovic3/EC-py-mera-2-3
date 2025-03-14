2046d790ac3a05acdd9049c5ea53d651ed187812
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-14 15:21:31 +0100

    Define controller method for getting heating fuel params

--- backend/controller/heating_fuel_controller.py
+++ backend/controller/heating_fuel_controller.py
@@ -1,6 +1,17 @@
 from backend.repository.heating_fuel_repository import HeatingFuelRepository
+from frontend.model.heating_fuel_parameters_model import HeatingFuelParameters
 
 
 class HeatingFuelController:
     def __init__(self):
         self.repository = HeatingFuelRepository()
+
+    def get_heating_fuel_parameters(
+        self,
+        heating_fuel_type: str,
+        heating_system_type: str
+    ) -> HeatingFuelParameters:
+        return self.repository.get_heating_fuel_parameters(
+            heating_fuel_type,
+            heating_system_type
+        )
