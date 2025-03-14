389dd980185ec1b52e5487cb8eab7822ca47cab0
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-14 14:14:45 +0100

    Define repository method for getting insulated surface params

--- backend/repository/insulated_surface_repository.py
+++ backend/repository/insulated_surface_repository.py
@@ -1,2 +1,12 @@
+from frontend.model.insulated_surface_parameters_model import InsulatedSurfaceParameters
+
 class InsulatedSurfaceRepository:
-    pass
+    def get_insulated_surface_parameters(
+        self,
+        insulated_surface_type: str,
+        construction_period: str
+    ) -> InsulatedSurfaceParameters:
+        insulated_surface_document = self.collection.find_one({"type": insulated_surface_type})
+        U = insulated_surface_document["U"][construction_period]
+        fxi = insulated_surface_document["fxi"]
+        return {U, fxi}
