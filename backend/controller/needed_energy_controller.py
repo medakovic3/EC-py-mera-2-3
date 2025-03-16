0b83c3fc33c4cdf77f8fd1ecce49a608ff7391a1
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-16 21:29:03 +0100

    Rename: needed_energy_repository -> repository

--- backend/controller/needed_energy_controller.py
+++ backend/controller/needed_energy_controller.py
@@ -2,7 +2,7 @@ from backend.repository.needed_energy_repository import NeededEnergyRepository
 
 class NeededEnergyController:
     def __init__(self):
-        self.needed_energy_repository = NeededEnergyRepository()
+        self.repository = NeededEnergyRepository()
 
     def get_needed(self, construction_period: str, dwelling_type: str) -> float:
-        return self.needed_energy_repository.get_needed(construction_period, dwelling_type)
+        return self.repository.get_needed(construction_period, dwelling_type)
