5bb0accdfc85f6e9523a5cd19370d0d1731b1b96
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-18 13:40:30 +0100

    Access db data through structure

--- frontend/component.py
+++ frontend/component.py
@@ -44,7 +44,9 @@ class Component:
 
     def load_hdd(self):
         municipality_name = self.user_home_info.municipality.value
-        self.hdd = self.municipality_service.get_hdd_by_municipality(municipality_name)
+        self.db_data.hdd = self.municipality_service.get_hdd_by_municipality(
+                                                        municipality_name
+                                                     )
 
     def load_needed_energy(self):
         construction_period = self.user_home_info.construction_period.value
@@ -55,7 +57,7 @@ class Component:
             building_type_str = self.user_home_info.building_type.value
             dwelling_type_str += " " + building_type_str
         
-        self.needed_energy_per_m2 = self.needed_energy_service.get_needed(
+        self.db_data.needed_energy_per_m2 = self.needed_energy_service.get_needed(
                                         construction_period,
                                         dwelling_type_str
                                     )       
@@ -63,7 +65,7 @@ class Component:
     def load_insulated_surface_parameters(self):
         insulated_surface_type = self.insulation_info.insulated_surface_type.value
         construction_period = self.user_home_info.construction_period.value
-        self.insulated_surface = \
+        self.db_data.insulated_surface = \
             self.insulated_surface_service.get_insulated_surface_parameters(
                 insulated_surface_type, construction_period
             )
@@ -71,10 +73,11 @@ class Component:
     def load_heating_fuel_parameters(self):
         heating_fuel_type = self.user_home_info.heating_fuel_type.value
         heating_system_type = self.user_home_info.heating_system_type.value
-        self.heating_fuel = self.heating_fuel_service.get_heating_fuel_parameters(
-            heating_fuel_type,
-            heating_system_type
-        )
+        self.db_data.heating_fuel = \
+            self.heating_fuel_service.get_heating_fuel_parameters(
+                                        heating_fuel_type,
+                                        heating_system_type
+                                      )
 
     def start_calculation(self):
         investment_cost: float = self.insulation_info.investment_cost
@@ -84,7 +87,7 @@ class Component:
     def annual_cost_savings(self) -> float:
         self.output_data.annual_cost_savings = \
             self.annual_final_energy_savings() * \
-            self.heating_fuel.consumption_per_kWh * \
+            self.db_data.heating_fuel.consumption_per_kWh * \
             self.user_home_info.fuel_cost_per_unit
         return self.output_data.annual_cost_savings
     
@@ -96,14 +99,15 @@ class Component:
         return self.output_data.annual_final_energy_savings
 
     def efficiency(self):
-        return  self.heating_fuel.efficiency.heating_fuel * \
-                self.heating_fuel.efficiency.pipe_system * \
-                self.heating_fuel.efficiency.pipe_regulation
+        return  self.db_data.heating_fuel.efficiency.heating_fuel * \
+                self.db_data.heating_fuel.efficiency.pipe_system * \
+                self.db_data.heating_fuel.efficiency.pipe_regulation
 
     def needed_energy_savings(self):
-        return  0.85 * self.insulated_surface.fxi * \
+        return  0.85 * self.db_data.insulated_surface.fxi * \
                 self.insulation_info.insulated_area * \
-                (self.insulated_surface.U - self.U_new()) * self.hdd * 24 / 1000
+                (self.db_data.insulated_surface.U - self.U_new()) * \
+                self.db_data.hdd * 24 / 1000
     
     def U_new(self):
         return 1 / self.R_new()
@@ -113,7 +117,7 @@ class Component:
                 self.insulation_info.insulation_thermal_conductivity
 
     def R_existing(self):
-        return 1 / self.insulated_surface.U
+        return 1 / self.db_data.insulated_surface.U
     
     def real_consumption_coef(self):
         return self.real_fuel_consumption() / self.calculated_fuel_consumption()
@@ -124,11 +128,11 @@ class Component:
                 self.calculated_fuel_consumption()
     
     def calculated_fuel_consumption(self):
-        return self.final_energy() / self.heating_fuel.consumption_per_kWh
+        return self.final_energy() / self.db_data.heating_fuel.consumption_per_kWh
     
     def final_energy(self):
         return self.needed_energy() / self.efficiency()
     
     def needed_energy(self):
-        return self.needed_energy_per_m2 * self.hdd / 2665.56 * \
+        return self.db_data.needed_energy_per_m2 * self.db_data.hdd / 2665.56 * \
                                                 self.user_home_info.floor_area
