66d58ee48a6dc567f594b8cdec0dce89aa7c50c4
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-21 13:55:41 +0100

    Calculate old CO2 emission

--- frontend/component.py
+++ frontend/component.py
@@ -218,3 +218,12 @@ class Component:
         needed_energy = needed_en_m2 * (hdd / hdd_average) * floor_area
 
         return needed_energy
+    
+    def co2_emission_old(self):
+        prim_en = self.primary_energy()
+        fuel_co2_em = self.db_data.heating_fuel.co2_emission
+        real_cons_coef = self.real_consumption_coef()
+
+        co2_em_old = prim_en * fuel_co2_em * real_cons_coef
+
+        return co2_em_old
