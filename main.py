24a8e934783263623f855638125b3f06bb3410a6
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-22 19:04:04 +0100

    Test boiler change savings

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from frontend.boiler.boiler_console import BoilerConsole
 from frontend.insulation.insulation_console import InsulationConsole
 
 def main():
-	console = InsulationConsole()
+	# console = InsulationConsole()
+	console = BoilerConsole()
 	console.run()
 
 if __name__ == "__main__":
