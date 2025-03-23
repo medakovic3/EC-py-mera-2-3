f20d782160b183d2a16e53ebeebc1b823d365653
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-23 18:37:18 +0100

    Test joinery change

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from frontend.boiler.boiler_console import BoilerConsole
 from frontend.insulation.insulation_console import InsulationConsole
+from frontend.joinery.joinery_console import JoineryConsole
 
 def main():
+	console = JoineryConsole()
 	# console = InsulationConsole()
-	console = BoilerConsole()
+	# console = BoilerConsole()
 	console.run()
 
 if __name__ == "__main__":
