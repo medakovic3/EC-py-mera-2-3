1c66fad3b3a07ae2ff6cc3fa1795f67540460d32
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-23 18:45:01 +0100

    Change the testing way

--- main.py
+++ main.py
@@ -2,11 +2,19 @@ from frontend.boiler.boiler_console import BoilerConsole
 from frontend.insulation.insulation_console import InsulationConsole
 from frontend.joinery.joinery_console import JoineryConsole
 
+joinery_console: JoineryConsole = None
+insulation_console: InsulationConsole = None
+boiler_console: BoilerConsole = None
+
 def main():
-	console = JoineryConsole()
-	# console = InsulationConsole()
-	# console = BoilerConsole()
-	console.run()
+	joinery_console = JoineryConsole()
+	joinery_console.run()
+
+	insulation_console = InsulationConsole()
+	insulation_console.run()
+
+	boiler_console = BoilerConsole()
+	boiler_console.run()
 
 if __name__ == "__main__":
 	main()
