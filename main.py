b5abbc871ecf43b207a311fc8b5074acfb96244a
Sava Stuparevic <stuparevicsava@gmail.com>
2025-03-26 11:40:13 +0100

    Make needed energy savings local variables

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from frontend.boiler.boiler_console import BoilerConsole
 from frontend.insulation.insulation_console import InsulationConsole
 from frontend.joinery.joinery_console import JoineryConsole
 
-joinery_needed_savings: float = 0.0
-insulation_needed_savings: float = 0.0
-
 def main():
+	joinery_needed_savings: float = 0.0
+	insulation_needed_savings: float = 0.0
+
 	joinery_console = JoineryConsole()
 	joinery_console.run()
 	joinery_needed_savings = joinery_console.component.needed_energy_savings()
